deb69782ffb934fa6222479493c82431c720410b
Shane-Donlon <130906067+Shane-Donlon@users.noreply.github.com>
2025-03-19 00:47:34 +0000

    add route announcer annoucment, and focus

--- src/components/router-head/routeAnnouncer.tsx
+++ src/components/router-head/routeAnnouncer.tsx
@@ -1,25 +1,62 @@
-import { component$, $, useOnDocument, useSignal, } from '@builder.io/qwik';
-import { useDocumentHead } from '@builder.io/qwik-city';
-
-
+import {
+	component$,
+	$,
+	useOnDocument,
+	useSignal,
+	useTask$,
+	isBrowser,
+} from "@builder.io/qwik";
+import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
 
 export const RouteAnnouncer = component$(() => {
-    const isSpa = useSignal<boolean>(false);
-    const doc = useDocumentHead();
+	const isSpa = useSignal<boolean>(false);
+	const loc = useLocation();
+	const doc = useDocumentHead().title;
+	const docSignal = useSignal<string | null>(doc);
+	useOnDocument(
+		"qsymbol",
+		$((e) => {
+			const isSpaEvent =
+				e.detail.symbol.includes("spaInit_event") ||
+				e.detail.symbol.includes("s_9KRx0IOCHt8");
+			if (isSpaEvent) {
+				isSpa.value = true;
+			}
+		}),
+	);
 
-    useOnDocument("qsymbol", $((e) => {
-        console.log("Event received", e.detail.symbol);
-        const isSpaEvent = e.detail.symbol.includes("spaInit_event") || e.detail.symbol.includes("s_9KRx0IOCHt8");
-        if (isSpaEvent) {
-            isSpa.value = true;
-        }
-
-    }))
-
-    return (
-        <>
-            {isSpa.value ? "true" : "false"}
-            {isSpa.value && <div role='status'>The current page is {doc.title}</div>}
-        </>
-    );
+	useTask$(({ track }) => {
+		track(() => {
+			doc;
+			loc;
+			if (doc) {
+				docSignal.value = doc;
+			} else {
+				docSignal.value = null;
+			}
+			if (loc.url.pathname === "/") {
+				docSignal.value = "home";
+			}
+			if (isBrowser) {
+				const a = document.querySelector(
+					"[data-qwik-route-announcer]",
+				) as HTMLElement;
+				if (a) {
+					a.setAttribute("tabindex", "1");
+					a.focus();
+					a.removeAttribute("tabindex");
+				}
+			}
+		});
+	});
+	return (
+		<>
+			{isSpa.value && (
+				<div role="status" aria-atomic="true" data-qwik-route-announcer>
+					Route Change - the current page is {docSignal.value ||
+						"document"}{" "}
+				</div>
+			)}
+		</>
+	);
 });
